9e60d7e57b7871d580f12ff27527a4bc31ebf309
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2019-10-22 15:55:07 -0300

    Consumo da API de fotos

---
+++ Projeto-fotos/fotos/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+    <li v-for="foto in fotos" :key="'foto'+foto.id">
+      
+      <img :src="foto.url" :alt="foto.titulo">
+    
+    </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+
+    return {
+      titulo: 'Galeria de Fotos',
+      fotos: []
+   }
+
+},
+
+  created() {
+    
+    let promise = this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos);    
+  }
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
